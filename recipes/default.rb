95509120238a7a07e9b6e639fd66e212ab1ac50d
Josh Richards <richards.joshua@gmail.com>
2014-08-09 22:05:14 -0500

    added kitchen-rackspace and fixed spelling error

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 #
 include_recipe 'platformstack::default'
 include_recipe 'rackops_rolebook::default'
-include_recipe 'phpstack::aplication_php'
+include_recipe 'phpstack::application_php'
 include_recipe 'phpstack::apache'
 include_recipe 'phpstack::mysql_base'
 include_recipe 'my_drupal_site::mysql'

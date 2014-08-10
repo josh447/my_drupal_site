394c0df7ba5a9468061cb5ad0ccc713e3961fa44
Josh Richards <richards.joshua@gmail.com>
2014-08-09 19:14:10 -0500

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# Encoding: utf-8
+name             'my_drupal_site'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'none'
+license          ''
+description      'Installs/Configures my_drupal_site'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'

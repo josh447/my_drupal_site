83914bda571697be10d785bf65fb9997435cecf7
Josh Richards <richards.joshua@gmail.com>
2014-08-09 23:17:35 -0500

    removed platformstack

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,5 @@ description      'Installs/Configures my_drupal_site'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends 'apt'
-depends 'platformstack'
 depends 'rackops_rolebook'
 depends 'phpstack'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,8 @@
 #
 # Copyright 2014, YOUR_COMPANY_NAME
 #
-include_recipe 'platformstack::default'
-include_recipe 'rackops_rolebook::default'
-include_recipe 'phpstack::application_php'
+include_recipe 'rackops_rolebook::rack_user'
 include_recipe 'phpstack::apache'
+include_recipe 'phpstack::application_php'
 include_recipe 'phpstack::mysql_base'
 include_recipe 'my_drupal_site::mysql'
